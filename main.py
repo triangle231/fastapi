933b4799c0ece90d5cda3f7cfd71f4ac7a1f1c5f
Triangle _ <132262998+triangle231@users.noreply.github.com>
2023-09-17 12:33:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
-import random
 import json
+import random
 
 app = FastAPI()
 
-class Item(BaseModel):
+class UserInput(BaseModel):
     system: str
     user: str
 
-@app.post("/api/gpt")
-def makeGPT(item: Item):
+@app.post("/makeGPT/")
+def make_gpt(input_data: UserInput):
     data = {
         "messages": [
             {
                 "role": "system",
-                "content": item.system
+                "content": input_data.system
             },
             {
                 "role": "user",
-                "content": item.user
+                "content": input_data.user
             }
         ],
         "stream": True,
         "model": "gpt-3.5-turbo",
-        "temperature" :0.7,
-        "presence_penalty" :0,
-        "frequency_penalty" :0,
-        'top_p': 1 
+        "temperature": 0.7,
+        "presence_penalty": 0,
+        "frequency_penalty": 0,
+        "top_p": 1 
     }
 
     while True:
-        urls = ["https://dongsiqie-gpt35.hf.space/api/openai/v1/chat/completions", 
-                'https://mikunakanoyyds-gptnextweb.hf.space/api/openai/v1/chat/completions']
-                
+        urls = ["https://dongsiqie-gpt35.hf.space/api/openai/v1/chat/completions", "https://mikunakanoyyds-gptnextweb.hf.space/api/openai/v1/chat/completions"]
         url = random.choice(urls)
 
         response = requests.post(url, data=json.dumps(data), stream=True)
 
-        allResult = ""
+        all_result = ""
 
         json_buffer = ''
-        
         for line in response.iter_lines():
             if line:
                 decoded_line = line.decode('utf-8')
-                
                 if decoded_line.startswith('data:'):
                     json_buffer += decoded_line[6:]
-                    
                     try:
                         json_line = json.loads(json_buffer)
                         content = json_line.get('choices', [{}])[0].get('delta', {}).get('content')
-                        
                         if content:
-                            allResult += content
-                        
+                            all_result = all_result + content
                         json_buffer = ''
-                        
                     except json.JSONDecodeError:
                         pass
-        
-                    
-        
-       if len(allResult) > 0:
-           break
-    
-    return {"response": allResult}
+        if len(all_result) < 1:
+            continue
+        break
+    return {"response": all_result}
